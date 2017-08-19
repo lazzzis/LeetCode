328d3a5c7412bf6abd963cf36718d0074d7bf644
lazzzis <shenlijin@outlook.com>
2017-08-19 11:52:54 -0400

    solve 005

--- 005/main.py
+++ 005/main.py
@@ -4,40 +4,34 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        memorize = dict()
-        def pad_test(start, end):
-            if (start, end) in memorize:
-                return memorize[(start, end)]
-            while start <= end:
-                if s[start] != s[end]:
-                    memorize[(start, end)] = False
-                    return False
-                memorize[(start, end)] = True
-                start, end = start + 1, end - 1
-            return True
-
-        n = len(s)
-        occurrence = {c:[] for c in s}
-        if len(occurrence) == 1:
-            return s
-
-        for i, c in enumerate(s):
-            occurrence[c].append(i)
-
-        ans = s[0]
+        dp = [[False for _ in s] for _ in s]
+        seats = {item:[] for item in s}
         start, end = 0, 0
+        for i, item in enumerate(s):
+            dp[i][i] = True
+        for i in range(len(s) - 1):
+            dp[i][i + 1] = s[i] == s[i + 1]
+            if dp[i][i + 1] and end - start < 1:
+                start, end = i, i + 1
 
-        for i in xrange(n):
-            i = n - 1 - i
-            c = s[i]
-            for pos in occurrence[c]:
-                if pos >= i or i - pos + 1 <= end - start + 1:
-                    break
-                if pad_test(pos + 1, i - 1):
-                    start, end = pos, i
-
+        for i, item in enumerate(s):
+            for j in seats[item]:
+                if i - j <= 1:
+                    continue
+                dp[j][i] = dp[j + 1][i - 1] and s[i] == s[j]
+                if dp[j][i] and i - j > end - start:
+                    start, end = j, i
+            seats[item].append(i)
+            # for j in range(0, i - 1):
+            #     dp[j][i] = dp[j + 1][i - 1] and s[i] == s[j]
+            #     if dp[j][i] and i - j > end - start:
+            #         start, end = j, i
         return s[start: end + 1]
 
-c = Solution()
+if __name__ == '__main__':
+    import os
+    if os.getenv('LZS'):
+        c = Solution()
 
-print c.longestPalindrome('a' * 1000)
+        print c.longestPalindrome('a' * 1000)
+        print c.longestPalindrome("rgczcpratwyqxaszbuwwcadruayhasynuxnakpmsyhxzlnxmdtsqqlmwnbxvmgvllafrpmlfuqpbhjddmhmbcgmlyeypkfpreddyencsdmgxysctpubvgeedhurvizgqxclhpfrvxggrowaynrtuwvvvwnqlowdihtrdzjffrgoeqivnprdnpvfjuhycpfydjcpfcnkpyujljiesmuxhtizzvwhvpqylvcirwqsmpptyhcqybstsfgjadicwzycswwmpluvzqdvnhkcofptqrzgjqtbvbdxylrylinspncrkxclykccbwridpqckstxdjawvziucrswpsfmisqiozworibeycuarcidbljslwbalcemgymnsxfziattdylrulwrybzztoxhevsdnvvljfzzrgcmagshucoalfiuapgzpqgjjgqsmcvtdsvehewrvtkeqwgmatqdpwlayjcxcavjmgpdyklrjcqvxjqbjucfubgmgpkfdxznkhcejscymuildfnuxwmuklntnyycdcscioimenaeohgpbcpogyifcsatfxeslstkjclauqmywacizyapxlgtcchlxkvygzeucwalhvhbwkvbceqajstxzzppcxoanhyfkgwaelsfdeeviqogjpresnoacegfeejyychabkhszcokdxpaqrprwfdahjqkfptwpeykgumyemgkccynxuvbdpjlrbgqtcqulxodurugofuwzudnhgxdrbbxtrvdnlodyhsifvyspejenpdckevzqrexplpcqtwtxlimfrsjumiygqeemhihcxyngsemcolrnlyhqlbqbcestadoxtrdvcgucntjnfavylip")
