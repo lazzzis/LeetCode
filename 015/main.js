c34d3f108d7fc329906031f6de049255ea9afb95
lazzzis <shenlijin@outlook.com>
2017-08-24 13:15:11 -0400

    015 in js

--- 015/main.js
+++ 015/main.js
@@ -3,50 +3,44 @@
  * @return {number[][]}
  */
 var threeSum = function (nums) {
-  nums = nums.sort()
-
-  const ans = new Set()
-  const indexes = {}
-  let mmax = -Infinity, mmin = Infinity
-
-  for (let i = nums.length - 1; i >= 0; i--) {
-    mmax = Math.max(mmax, nums[i])
-    mmin = Math.min(mmin, nums[i])
-    if (!(nums[i] in indexes)) {
-      indexes[nums[i]] = i
-    }
-  }
-
-  for (let i1 = 0; i1 < nums.length; i1++) {
-    for (let i2 = i1 + 1; i2 < nums.length; i2++) {
-      const n1 = nums[i1]
-      const n2 = nums[i2]
-      const n3 = 0 - n1 - n2
-      const i3 = indexes[n3]
-      if (i3 !== -1 && i3 > i2) {
-        ans.add([n1, n2, n3].sort().join(' '))
+  function twoSum (start, end, sum) {
+    const ans = []
+    let lv = nums[0] - 1
+    let rv = nums[0] - 1
+    while (start < end) {
+      lv = nums[start]
+      rv = nums[end]
+      if (lv + rv === sum) {
+        ans.push([-sum, lv, rv])
+        while (start < end && nums[start] === lv && nums[end] === rv) {
+          start++
+          end--
+        }
+      } else if (lv + rv > sum) {
+        while (start < end && nums[end] === rv) {
+          end--
+        }
+      } else if (lv + rv < sum) {
+        while (start < end && nums[start] === lv) {
+          start++
+        }
       }
     }
+    return ans
   }
-  return Array.from(ans).map(x => x.split(' ').map(Number))
+  let ans = []
+  nums.sort((x, y) => x < y ? -1 : 1)
+  nums.forEach((item, index) => {
+    if (index > 0 && nums[index - 1] === item) {
+      return
+    }
+    const twos = twoSum(index + 1, nums.length - 1, -item)
+    if (twos.length) {
+      ans = ans.concat(twos)
+    }
+  })
+  return ans
 }
 
-if (require.main === module) {
-  const assert = require('assert')
-  assert.deepStrictEqual(threeSum([-1, 0, 1, 2, -1, -4]), [
-    [-1, 0, 1],
-    [-1, -1, 2]
-  ])
-
-  assert.deepStrictEqual(threeSum([0, 0, 0]), [
-    [0, 0, 0]
-  ])
-
-  assert.deepStrictEqual(threeSum([1, 2, 3]), [])
-
-  assert.deepStrictEqual(threeSum([1, 2, 3, 1, -2, -1, -1, 2]), [
-    [-2, 1, 1],
-    [-1, -1, 2],
-    [-1, -2, 3]
-  ])
-}
+console.log(threeSum([-1, 0, 1, 2, -1, -4]))
+console.log(threeSum([-3, 4, 4, 8, -4, -4, 1, 2]))
