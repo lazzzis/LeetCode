1d9093c015c4f4817b599f14bedc39927faf707f
lazzzis <shenlijin@outlook.com>
2017-09-03 23:45:06 -0400

    042

---
+++ 042/main.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def trap(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        left, right = 0, len(height) - 1
+        lmax, rmax = -1, -1
+        res = 0
+        while left > right:
+            lv, rv = height[left], height[right]
+            lmax, rmax = max(lmax, lv), max(rmax, rv)
+            if lmax < rmax:
+                res += lmax - lv
+                left += 1
+            else:
+                res += rmax - rv
+                right -= 1
+        return res
