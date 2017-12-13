12bf42489081644c42a99d16245adac175ff95fd
lazzzis <shenlijin@outlook.com>
2017-12-13 13:49:50 -0500

    398 java2

---
+++ 398/Solution2.java
@@ -0,0 +1,26 @@
+class Solution2 {
+
+    private Random random;
+    private Map<Integer, List<Integer>> map;
+
+    public Solution(int[] nums) {
+        this.random = new Random();
+        this.map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            List<Integer> list = map.getOrDefault(nums[i], new ArrayList<>());
+            list.add(i);
+            map.put(nums[i], list);
+        }
+    }
+
+    public int pick(int target) {
+        List<Integer> list = map.get(target);
+        return list.get(random.nextInt(list.size()));
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(nums);
+ * int param_1 = obj.pick(target);
+ */
