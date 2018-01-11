35f0592de0a53041eca594c4bfeaec1731430ac5
lazzzis <shenlijin@outlook.com>
2018-01-11 12:26:48 -0500

    101

---
+++ 101/main.go
@@ -0,0 +1,19 @@
+func isSymmetric(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	return helper(root.Left, root.Right)
+}
+
+func helper(left *TreeNode, right *TreeNode) bool {
+	if left == nil && right == nil {
+		return true
+	}
+	if left == nil || right == nil {
+		return false
+	}
+	if left.Val != right.Val {
+		return false
+	}
+	return helper(left.Left, right.Right) && helper(left.Right, right.Left)
+}
