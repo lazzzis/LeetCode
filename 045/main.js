370a356dab2242163f7f7a5283b9c5d105e692a4
lazzzis <shenlijin@outlook.com>
2018-01-19 18:08:11 -0500

    045

--- 045/main.js
+++ 045/main.js
@@ -2,31 +2,21 @@
  * @param {number[]} nums
  * @return {number}
  */
-var jump = function (nums) {
-  const steps = {}
-  let reach = 0
-  steps[0] = 0
-
-  for (let index = 0; index < nums.length; index++) {
-    const item = nums[index]
-    const step = steps[index]
-    const start = reach + 1
-    const end = Math.min(nums.length, index + item + 1)
-    for (let i = start; i < end; i++) {
-      steps[i] = step + 1
+var jump = function(nums) {
+  if (nums.length <= 1) return 0
+  let step = 0
+  let curMax = 0
+  let nextMax = 0
+  let i = 0
+  while (i < nums.length) {
+    curMax = nextMax
+    while (i < nums.length && i <= curMax) {
+      nextMax = Math.max(nextMax, i + nums[i])
+      i++
     }
-    reach = Math.max(reach, index + item)
-    if (end >= nums.length) {
-      break
-    }
+    step++
+    curMax = nextMax
+    if (curMax >= nums.length - 1) return step
   }
-
-  return steps[nums.length - 1]
-}
-
-if (process.env.LZS) {
-  // local test
-  console.log(jump([3, 1, 2, 4]))
-  console.log(jump([3, 1, 2, 5, 4]))
-  console.log(jump([3]))
-}
+  return step
+};
